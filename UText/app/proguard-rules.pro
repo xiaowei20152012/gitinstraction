638e66caba0cf4e5a5c4c0d8fd64d1bf2be8fe0a
liwei <wei.li@zenjoy.net>
2018-03-23 15:13:32 +0800

    [LW] add umedia

--- UText/app/proguard-rules.pro
+++ UText/app/proguard-rules.pro
@@ -1,9 +1,13 @@
 # Add project specific ProGuard rules here.
-# You can control the set of applied configuration files using the
-# proguardFiles setting in build.gradle.
+# By default, the flags in this file are appended to flags specified
+# in /Users/aidanfollestad/Documents/android-sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
 #
 # For more details, see
 #   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
 
 # If your project uses WebView with JS, uncomment the following
 # and specify the fully qualified class name to the JavaScript interface
@@ -12,10 +16,41 @@
 #   public *;
 #}
 
-# Uncomment this to preserve the line number information for
-# debugging stack traces.
-#-keepattributes SourceFile,LineNumberTable
+# RetroFit
+-dontwarn retrofit.**
+-keep class retrofit.** { *; }
+-keepattributes Signature
+-keepattributes Exceptions
 
-# If you keep the line number information, uncomment this to
-# hide the original source file name.
-#-renamesourcefileattribute SourceFile
+# Glide
+-keep public class * implements com.bumptech.glide.module.GlideModule
+-keep public enum com.bumptech.glide.load.resource.bitmap.ImageHeaderParser$** {
+    **[] $VALUES;
+    public *;
+}
+
+# ButterKnife
+-keep class butterknife.** { *; }
+-dontwarn butterknife.internal.**
+-keep class **$$ViewBinder { *; }
+-keepclasseswithmembernames class * {
+    @butterknife.* <fields>;
+}
+-keepclasseswithmembernames class * {
+    @butterknife.* <methods>;
+}
+
+-keep class !android.support.v7.internal.view.menu.**,** {*;}
+
+# optimize Log
+-assumenosideeffects class android.util.Log {
+    public static *** d(...);
+    public static *** e(...);
+    public static *** i(...);
+    public static *** v(...);
+    public static *** w(...);
+}
+# end optimize Log
+
+-dontwarn
+-ignorewarnings
---
+++ UText/umedia/src/main/java/com/umedia/android/App.java
@@ -0,0 +1,86 @@
+package com.umedia.android;
+
+import android.app.Application;
+
+
+public class App extends Application {
+
+    public static final String TAG = App.class.getSimpleName();
+
+//    public static final String GOOGLE_PLAY_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjMeADN5Ffnt/ml5SYxNPCn8kGcOYGpHEfNSCts99vVxqmCn6C01E94c17j7rUK2aeHur5uxphZylzopPlQ8P8l1fqty0GPUNRSo18FCJzfGH8HZAwZYOcnRFPaXdaq3InyFJhBiODh2oeAcVK/idH6QraQ4r9HIlzigAg6lgwzxl2wJKDh7X/GMdDntCyzDh8xDQ0wIawFgvgojHwqh2Ci8Gnq6EYRwPA9yHiIIksT8Q30QyM5ewl5QcnWepsls7enNqeHarhpmSibRUDgCsxHoOpny7SyuvZvUI3wuLckDR0ds9hrt614scHHqDOBp/qWCZiAgOPVAEQcURbV09qQIDAQAB";
+//    public static final String PRO_VERSION_PRODUCT_ID = "pro_version";
+
+    private static App app;
+    // pay about
+//    private BillingProcessor billingProcessor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+        // default theme
+        if (!ThemeStore.isConfigured(this, 1)) {
+            ThemeStore.editTheme(this)
+                    .activityTheme(R.style.Theme_Phonograph_Light)
+                    .primaryColorRes(R.color.md_indigo_500)
+                    .accentColorRes(R.color.md_pink_A400)
+                    .commit();
+        }
+
+        // Set up Crashlytics, disabled for debug builds
+//        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+//                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+//                .build();
+//        if (!BuildConfig.DEBUG) {
+//            Fabric.with(this, crashlyticsKit, new Answers());
+//        } else {
+//            Fabric.with(this, crashlyticsKit, new Crashlytics(), new Crashlytics()); // crashlytics kit is disabled here
+//        }
+
+        // Set up dynamic shortcuts
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            new DynamicShortcutManager(this).initDynamicShortcuts();
+        }
+
+        // automatically restores purchases
+//        billingProcessor = new BillingProcessor(this, App.GOOGLE_PLAY_LICENSE_KEY, new BillingProcessor.IBillingHandler() {
+//            @Override
+//            public void onProductPurchased(String productId, TransactionDetails details) {
+//            }
+//
+//            @Override
+//            public void onPurchaseHistoryRestored() {
+////                Toast.makeText(App.this, R.string.restored_previous_purchase_please_restart, Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onBillingError(int errorCode, Throwable error) {
+//            }
+//
+//            @Override
+//            public void onBillingInitialized() {
+//            }
+//        });
+    }
+
+    public static boolean isProVersion() {
+//        return BuildConfig.DEBUG || app.billingProcessor.isPurchased(PRO_VERSION_PRODUCT_ID);
+        return BuildConfig.DEBUG;
+    }
+
+    public static App getInstance() {
+        return app;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+//        billingProcessor.release();
+    }
+}
